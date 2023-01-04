d12e8a9ad12831db4fdf1b26185a0f2257551258
billywba <billy.wba8@gmail.com>
2023-01-04 11:01:31 +0000

    added room to each exam

--- src/chromosome.py
+++ src/chromosome.py
@@ -4,10 +4,11 @@ class Chromosome:
     def __init__(self, student_units, tutors, units):
         self.AMOUNT_OF_DAYS = 5
         self.EXAMS_PER_DAY = 2
+        self.rooms = ['P4' + str(i) for i in range(11, 21)]
 
         self.schedule = []
         for i in range (0, self.AMOUNT_OF_DAYS * self.EXAMS_PER_DAY):
-            self.schedule.append([random.choice(student_units), random.choice(tutors), random.choice(units)])
+            self.schedule.append([random.choice(self.rooms), random.choice(student_units), random.choice(tutors), random.choice(units)])
 
     def print_schedule(self):
         days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday']
