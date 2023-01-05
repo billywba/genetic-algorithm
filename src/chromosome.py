844569e9a0169624e23b12e52fd12334eb957435
billywba <billy.wba8@gmail.com>
2023-01-05 13:02:27 +0000

    minor changes

--- src/chromosome.py
+++ src/chromosome.py
@@ -25,7 +25,7 @@ class Chromosome:
 
         self.schedule = []
         for i in range (0, self.AMOUNT_OF_DAYS * self.EXAMS_PER_DAY):
-            self.schedule.append([random.choice(self.rooms), random.choice(units), random.choice(tutors), random.choice(units)])
+            self.schedule.append([random.choice(self.rooms), random.choice(units), random.choice(tutors)])
 
     def evaluate_fitness(self):
         hard_constraint_functions = [
@@ -127,6 +127,6 @@ class Chromosome:
             morning_unit = self.schedule[i * 2]
             evening_unit = self.schedule[i * 2 + 1]
             
-            print("| {:<61} |".format(days[i]))
-            print("| Room | Morning Units | Tutors | Room | Evening Units | Tutors |")
+            print("| {:<62} |".format(days[i]))
+            print("| Room | Morning Units | Tutors  | Room | Evening Units | Tutors |")
             print("| {:<4} | {:<13} | {:<6} | {:<4} | {:<13} | {:<6} |\n".format(morning_unit[0], morning_unit[1], morning_unit[2], evening_unit[0], evening_unit[1], evening_unit[2]))
