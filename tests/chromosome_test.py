6f35bf6616166ff108d26c71e6243a904cff3cae
billywba <billy.wba8@gmail.com>
2023-01-08 19:11:40 +0000

    added mutate test

--- tests/chromosome_test.py
+++ tests/chromosome_test.py
@@ -1,4 +1,5 @@
 import unittest
+import random
 
 from src.chromosome import Chromosome
 
@@ -9,3 +10,22 @@ class ChromosomeTest(unittest.TestCase):
                                 ['CIS311', 'CIS312', 'CIS313'])
 
         self.assertEqual(len(chromosome.schedule), 10)
+
+    def test_mutate(self):
+        # Set seed to reproduce same results
+        random.seed(42)
+        
+        chromosome = Chromosome([['1', 'test_student', 'CIS312'], ['2', 'test_student2', 'CIS312'], ['3', 'test_student3', 'CIS312']], 
+                                ['Tutor1', 'Tutor2', 'Tutor3', 'Tutor4'], 
+                                ['CIS311', 'CIS312', 'CIS313'])
+
+        chromosome.schedule = [
+                                ['P417', 'CIS311', 'Tutor5'],
+                                ['P418', 'CIS312', 'Tutor3']
+                            ]
+
+        # Mutate with 100% probability
+        chromosome.mutate(1)
+
+        # Assert new schedule has changed evening unit (from CIS312 to CIS311)
+        self.assertEqual(chromosome.schedule, [['P417', 'CIS311', 'Tutor5'], ['P418', 'CIS311', 'Tutor3']])
