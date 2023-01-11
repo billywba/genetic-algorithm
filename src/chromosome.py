4777ef3d0fb6d8ff398e323b458f3425a1feccbf
billywba <billy.wba8@gmail.com>
2023-01-11 13:47:39 +0000

    updated print_schedule()

--- src/chromosome.py
+++ src/chromosome.py
@@ -141,14 +141,13 @@ class Chromosome:
                 # Change tutor
                 self.schedule[mutated_exam_index][2] = random.choice(self.tutors)
 
-
     def print_schedule(self):
-        days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday']
+        for exam_time in self.exam_times:
+            print(exam_time)
+            print("| Room | Unit   | Tutors  |")
 
-        for i in range(0, len(days)):
-            morning_unit = self.schedule[i * 2]
-            evening_unit = self.schedule[i * 2 + 1]
-            
-            print("| {:<62} |".format(days[i]))
-            print("| Room | Morning Units | Tutors  | Room | Evening Units | Tutors |")
-            print("| {:<4} | {:<13} | {:<6} | {:<4} | {:<13} | {:<6} |\n".format(morning_unit[0], morning_unit[1], morning_unit[2], evening_unit[0], evening_unit[1], evening_unit[2]))
+            for exam in self.schedule:
+                if exam[3] == exam_time:
+                    print("| {:<4} | {:<6} | {:<6} |".format(exam[0], exam[1], exam[2]))
+
+            print("\n")
